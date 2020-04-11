2fba5d04292ee4cb2c3cf01b51ed9b030e7e0fdd
Shubhankar Das <shubhankar@browserstack.com>
2020-04-11 22:17:00 +0530

    removed contacct

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Shubhankar, a full-stack developer living in beautiful Pune</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me.</Link>
-      </p>
     </Layout>
   )
 }
